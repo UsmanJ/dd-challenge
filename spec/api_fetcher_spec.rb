a33bb855037572cfa35f1518e618eb8862c71c8a
ALRW <awerner1@googlemail.com>
2016-02-16 11:52:40 +0000

    added first test

---
+++ spec/api_fetcher_spec.rb
@@ -0,0 +1,13 @@
+decribe ApiFetcher do
+
+  let(:key) { '123456' }
+  subject(:apiFetch) { described_class.new() }
+
+  context '#url_builder' do
+
+    it 'constructs the api url' do
+      expect(apiFetch.build_url).to eq('https://openapi.etsy.com/v2/listings/active?api_key=n6mbi422ip24opnbsin32bk9')
+    end
+  end
+
+end
